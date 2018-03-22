679bb5120df2218034973e7cdaed9d7b7338f00f
rghmyr <napium.xda@gmail.com>
2018-03-22 14:22:47 +0100

    angesichert durch ip sperre

--- standards/include/usercontrol.php
+++ standards/include/usercontrol.php
@@ -4,5 +4,8 @@
   $mysqli = new mysqli($db['host'], $db['username'], $db['password'], $db['database']);
 
   $_SESSION['backend-user'] = "admin";
-  $_SESSION['backend-role'] = "admin";
+  if(strpos($_SERVER['REMOTE_ADDR'],"::1")!==false)
+    $_SESSION['backend-role'] = "admin";
+  else
+    $_SESSION['backend-role'] = "other";
 ?>
